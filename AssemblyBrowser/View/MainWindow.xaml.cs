bb7e3814e90cf99ee1809002c4e29bdfdf0386dd
artem0189 <56150204+artem0189@users.noreply.github.com>
2020-09-30 12:38:39 +0300

    Create MVVM structure

--- AssemblyBrowser/View/MainWindow.xaml.cs
+++ AssemblyBrowser/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AssemblyBrowser
+namespace AssemblyBrowser.View
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
