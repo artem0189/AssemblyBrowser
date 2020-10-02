3d114588ee225ab2ae9599cff18350fb2250d9fe
artem0189 <56150204+artem0189@users.noreply.github.com>
2020-10-02 20:44:43 +0300

    Init TestLibrary

--- AssemblyLib/Reflection/Metadata.cs
+++ AssemblyLib/Reflection/Metadata.cs
@@ -26,33 +26,54 @@ namespace AssemblyLib.Reflection
 
         internal static List<IAssemblyNode> GetMethods(Type type)
         {
+            string name = "";
             List<IAssemblyNode> result = new List<IAssemblyNode>();
             MethodInfo[] methods = type.GetMethods(_flags | BindingFlags.DeclaredOnly);
             for (int i = 0; i < methods.Length; i++)
             {
-                result.Add(new MethodNode(methods[i].Name));
+                name = GetMethodSignature(methods[i]);
+                result.Add(new MethodNode(name));
             }
             return result;
         }
 
+        private static string GetMethodSignature(MethodInfo method)
+        {
+            string result = method.ReturnType.Name + " " + method.Name + "(";
+            ParameterInfo[] parameters = method.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                result += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                if (i < parameters.Length - 1)
+                {
+                    result += ", ";
+                }
+            }
+            return result + ")";
+        }
+
         internal static List<IAssemblyNode> GetFields(Type type)
         {
+            string name = "";
             List<IAssemblyNode> result = new List<IAssemblyNode>();
             FieldInfo[] fields = type.GetFields(_flags);
             for (int i = 0; i < fields.Length; i++)
             {
-                result.Add(new FieldNode(fields[i].Name));
+                name = fields[i].FieldType.Name + " " + fields[i].Name;
+                result.Add(new FieldNode(name));
             }
             return result;
         }
 
         internal static List<IAssemblyNode> GetProperties(Type type)
         {
+            string name = "";
             List<IAssemblyNode> result = new List<IAssemblyNode>();
             PropertyInfo[] properties = type.GetProperties(_flags);
             for (int i = 0; i < properties.Length; i++)
             {
-                result.Add(new PropertyNode(properties[i].Name));
+                name = properties[i].PropertyType.Name + " " + properties[i].Name;
+                result.Add(new PropertyNode(name));
             }
             return result;
         }
